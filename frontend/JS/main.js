d8cf336d8b1e04329346385259256e090ebdd9f5
Savchenko Andriy <savchenko.andriy.mind@gamil.com>
2019-04-15 01:42:01 +0300

    some adaptive fix

--- frontend/JS/main.js
+++ frontend/JS/main.js
@@ -10,6 +10,8 @@ $(function(){
     // робим маски плагіном
     $('#user_phone-one').mask('+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9' , {autoclear: false});
     $('#user_phone-two').mask('+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9' , {autoclear: false});
+    $('#user_phone-three').mask('+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9' , {autoclear: false});
     $('#text').mask({autoclear: false});
     $('#text-one').mask({autoclear: false});
+    $('#text-three').mask({autoclear: false});
 });
